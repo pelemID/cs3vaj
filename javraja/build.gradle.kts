a52704c5ac8886b9583873a12684db849d9eda99
pelemID <kodelz@gmail.com>
2025-10-08 15:20:19 +0700

    Add files via upload

---
+++ javraja/build.gradle.kts
@@ -0,0 +1,38 @@
+
+//import org.jetbrains.kotlin.konan.properties.Properties
+// use an integer for version numbers
+version = 1
+
+android {
+    namespace = "javraja"
+//    defaultConfig {
+//        val properties = Properties()
+//        properties.load(project.rootProject.file("local.properties").inputStream())
+//        android.buildFeatures.buildConfig=true
+//        buildConfigField("String", "KissKh", "\"${properties.getProperty("KissKh")}\"")
+//        buildConfigField("String", "KisskhSub", "\"${properties.getProperty("KisskhSub")}\"")
+//    }
+}
+
+
+cloudstream {
+    language = "id"
+    // All of these properties are optional, you can safely remove them
+
+     description = "Jav Raja"
+     authors = listOf("Phisher98")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf("NSFW")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=kisskh.co&sz=128"
+
+//    isCrossPlatform = true
+}
---
+++ javraja/src/main/kotlin/com/javraja/javraja.kt
@@ -0,0 +1,162 @@
+package com.javraja
+
+import android.util.Base64
+import com.lagradost.api.Log
+//import android.util.Log
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+
+class javraja : MainAPI() {
+    override var mainUrl              = "https://rajajav.com"
+    override var name                 = "Javraja"
+    override val hasMainPage          = true
+    override var lang                 = "en"
+    override val supportedTypes       = setOf(TvType.NSFW)
+    override val vpnStatus            = VPNStatus.MightBeNeeded
+
+    override val mainPage = mainPageOf(
+        // Fixed entries
+            "" to "Main Page",
+            "jav-sub-indo-terbaru" to "Terbaru",
+            //"category/english-subbed/?orderby=likes&order=DESC" to "English Subbed most likes",
+            //"tag/for-women" to "For Women",
+            //"actress/hanamiya-amu" to "Amu",
+    )
+
+    
+ override suspend fun getMainPage(
+        page: Int,
+        request: MainPageRequest
+    ): HomePageResponse {
+        val document = app.get("$mainUrl/${request.data}/").document
+        val home =  document.select("div.bx").map { it.toSearchResult() }
+        return newHomePageResponse(
+            list = HomePageList(
+                name = request.name,
+                list = home,
+                isHorizontalImages = true
+            ),
+            hasNext = true
+        )
+    }
+
+    private fun Element.toSearchResult(): SearchResponse {
+        val title     = this.select("a.tip").attr("title")
+        val href      = fixUrl(this.select("a.tip").attr("href"))
+        val posterUrl = fixUrlNull(this.select("a.tip > img").attr("src"))
+        return newMovieSearchResponse(title, href, TvType.NSFW) {
+            this.posterUrl = posterUrl
+        }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val searchResponse = mutableListOf<SearchResponse>()
+
+        for (i in 1..3) {
+            val document = app.get("${mainUrl}/page/$i/?s=$query").document
+            val results = document.select("#main > div").mapNotNull { it.toSearchResult() }
+            if (!searchResponse.containsAll(results)) {
+                searchResponse.addAll(results)
+            } else {
+                break
+            }
+            if (results.isEmpty()) break
+        }
+        return searchResponse
+    }
+
+
+    private suspend fun findposAct(namanya: String) : String {
+        val encodedName = namanya.trim().replace(" ", "+")
+        val document = app.get("${mainUrl}/actress-search/?taxonomy_search=$encodedName").document
+        
+        // find <img> where alt == namanya
+        return document.selectFirst("div.actress-pic img[alt=\"$namanya\"]")
+                ?.attr("src")
+                .orEmpty()
+    }
+
+        
+    override suspend fun load(url: String): LoadResponse {
+        val document = app.get(url).document
+
+        val title= document.selectFirst("div.posts > h1")?.text().toString()
+        val poster = document.selectFirst("div.wp-content > p > img")
+                    ?.attr("src")
+                    ?.trim()
+                    ?: document.selectFirst("div.large-screenimg > img")
+                    ?.attr("src")
+                    ?.trim()
+                    .orEmpty()
+        val description = document.selectFirst("div.wp-content p")
+                    ?.text()
+                    ?.trim()
+                    ?: document.select("li:has(strong:matchesOwn(Actress)) a")
+                    ?.eachText()
+                    ?.joinToString(", ")
+                    .orEmpty()
+
+         val actors = document.select("li:has(strong:matchesOwn(Actress)) a").take(8).map {
+                    Actor(
+                            it.text(),
+                            findposAct(it.text())
+                    )
+                }
+                    
+        //val actress = cariArtis(document.select("li:has(strong:matchesOwn(Actress)) a")?.eachText()
+        return newMovieLoadResponse(title, url, TvType.NSFW, url) {
+            this.posterUrl = poster
+            this.plot      = description
+            addActors(actors)
+        }
+    }
+
+     override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+        val document = app.get(data).document
+        val script=document.select("script:containsData(iframe_url)").html()
+        val IFRAME_B64_REGEX = Regex(""""iframe_url":"([^"]+)"""")
+         val iframeUrls = IFRAME_B64_REGEX.findAll(script)
+             .map { it.groupValues[1] }
+             .map { Base64.decode(it, Base64.DEFAULT).let(::String) }
+             .toList()
+         iframeUrls.forEach {
+             Log.d("Phisher",it)
+             val iframedoc=app.get(it, referer = it).document
+             val olid=iframedoc.toString().substringAfter("var OLID = '").substringBefore("'")
+             val newreq=iframedoc.toString().substringAfter("iframe").substringAfter("src=\"").substringBefore("'+OLID")
+             val reverseid= olid.edoceD()
+             val location= app.get("$newreq$reverseid", referer = it, allowRedirects = false)
+             val link=location.headers["location"].toString()
+             if (link.contains(".m3u"))
+             {
+                 callback.invoke(
+                     newExtractorLink(
+                         source = name,
+                         name = name,
+                         url = link,
+                         INFER_TYPE
+                     ) {
+                         this.referer = ""
+                         this.quality = getQualityFromName("")
+                     }
+                 )
+             }
+             else{
+                 loadExtractor(link, referer = it,subtitleCallback,callback)
+             }
+         }
+        return true
+    }
+
+    fun String.edoceD(): String {
+        var x = this.length - 1
+        var edoceD = ""
+        while (x >= 0) {
+            edoceD += this[x]
+            x--
+        }
+        return edoceD
+    }
+}
