6e1556e929e67e12093cef937779f4a7fa0d5542
pelemID <kodelz@gmail.com>
2025-11-04 14:28:18 +0700

    Update build.gradle.kts

--- javID/build.gradle.kts
+++ javID/build.gradle.kts
@@ -5,7 +5,7 @@ version = 1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Jav Sub Indo"
+    description = "Nge Jav Sub Indo"
     language    = "id"
     authors = listOf("Phisher98")
 
