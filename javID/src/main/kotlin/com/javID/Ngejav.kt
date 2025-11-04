603edcfda4e257135d6a3b0858a3935691f87fd8
pelemID <kodelz@gmail.com>
2025-11-04 15:10:45 +0700

    Update Ngejav.kt

--- javID/src/main/kotlin/com/javID/Ngejav.kt
+++ javID/src/main/kotlin/com/javID/Ngejav.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 
-class Javsubid : MainAPI() {
+class Ngejav : MainAPI() {
     override var mainUrl              = "https://ngejav.life"
     override var name                 = "Javsubid"
     override val hasMainPage          = true
